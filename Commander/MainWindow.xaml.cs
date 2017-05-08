38369b3750ee959e4040c9f983aee1b92cc3bee7
Uwe Riegel <uwriegel@gmail.com>
2017-05-08 19:37:38 +0200

    Save Window settings, Icon

--- Commander/MainWindow.xaml.cs
+++ Commander/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Commander.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,57 @@ namespace Commander
         public MainWindow()
         {
             InitializeComponent();
+            if (Settings.Default.WindowTop != 0 ||
+                Settings.Default.WindowLeft != 0 ||
+                Settings.Default.WindowHeight != 0 ||
+                Settings.Default.WindowWidth != 0)
+            {
+                Top = Settings.Default.WindowTop;
+                Left = Settings.Default.WindowLeft;
+                Height = Settings.Default.WindowHeight;
+                Width = Settings.Default.WindowWidth;
+                WindowState = Settings.Default.WindowState;
+            }
+            SizeToFit();
+            MoveIntoView();
+        }
+
+        void SizeToFit()
+        {
+            if (Height > SystemParameters.VirtualScreenHeight)
+                Height = SystemParameters.VirtualScreenHeight;
+
+            if (Width > SystemParameters.VirtualScreenWidth)
+                Width = SystemParameters.VirtualScreenWidth;
+        }
+
+        void MoveIntoView()
+        {
+            if (Top + Height / 2 > SystemParameters.VirtualScreenHeight)
+                Top = SystemParameters.VirtualScreenHeight - Height;
+
+            if (Left + Width / 2 > SystemParameters.VirtualScreenWidth)
+                Left = SystemParameters.VirtualScreenWidth - Width;
+
+            if (Top < 0)
+                Top = 0;
+
+            if (Left < 0)
+                Left = 0;
+        }
+
+        void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (WindowState != WindowState.Minimized)
+            {
+                Settings.Default.WindowTop = Top;
+                Settings.Default.WindowLeft = Left;
+                Settings.Default.WindowHeight = Height;
+                Settings.Default.WindowWidth = Width;
+                Settings.Default.WindowState = WindowState;
+
+                Settings.Default.Save();
+            }
         }
     }
 }
---
+++ Commander/Preferences.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Commander
+{
+    class Preferences
+    {
+        public double WindowTop { get; set; }
+        public double WindowLeft { get; set; }
+        public double WindowHeight { get; set; }
+        public double WindowWidth { get; set; }
+        public WindowState WindowState { get; set; }
+
+        public Preferences()
+        {
+            Load();
+            SizeToFit();
+            MoveIntoView();
+        }
+
+        public void Save()
+        {
+            if (WindowState != System.Windows.WindowState.Minimized)
+            {
+                Properties.Settings.Default.WindowTop = WindowTop;
+                Properties.Settings.Default.WindowLeft = WindowLeft;
+                Properties.Settings.Default.WindowHeight = WindowHeight;
+                Properties.Settings.Default.WindowWidth = WindowWidth;
+                Properties.Settings.Default.WindowState = WindowState;
+
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        public void SizeToFit()
+        {
+            if (WindowHeight > System.Windows.SystemParameters.VirtualScreenHeight)
+                WindowHeight = System.Windows.SystemParameters.VirtualScreenHeight;
+
+            if (WindowWidth > System.Windows.SystemParameters.VirtualScreenWidth)
+                WindowWidth = System.Windows.SystemParameters.VirtualScreenWidth;
+        }
+
+        public void MoveIntoView()
+        {
+            if (WindowTop + WindowHeight / 2 > System.Windows.SystemParameters.VirtualScreenHeight)
+                WindowTop = System.Windows.SystemParameters.VirtualScreenHeight - WindowHeight;
+
+            if (WindowLeft + WindowWidth / 2 > System.Windows.SystemParameters.VirtualScreenWidth)
+                WindowLeft = System.Windows.SystemParameters.VirtualScreenWidth - WindowWidth;
+            
+            if (WindowTop < 0)
+                WindowTop = 0;
+
+            if (WindowLeft < 0)
+                WindowLeft = 0;
+        }
+
+        void Load()
+        {
+            if (Properties.Settings.Default.WindowTop != 0 &&
+                Properties.Settings.Default.WindowLeft != 0 &&
+                Properties.Settings.Default.WindowHeight != 0 &&
+                Properties.Settings.Default.WindowWidth != 0)
+            {
+                WindowTop = Properties.Settings.Default.WindowTop;
+                WindowLeft = Properties.Settings.Default.WindowLeft;
+                WindowHeight = Properties.Settings.Default.WindowHeight;
+                WindowWidth = Properties.Settings.Default.WindowWidth;
+                WindowState = Properties.Settings.Default.WindowState;
+            }
+        }
+    }
+}
